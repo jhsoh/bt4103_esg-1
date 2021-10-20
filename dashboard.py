925e67cd5b5b5de06637ff34aae19c43918974bf
xxalicia <79690920+xxalicia@users.noreply.github.com>
2021-10-20 17:58:16 +0800

    added more features into dashboard

--- dashboard.py
+++ dashboard.py
@@ -11,76 +11,138 @@ import dash_core_components as dcc
 import dash.dependencies as dd
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc 
+import dash_daq as daq
 
 from io import BytesIO
-
 from wordcloud import WordCloud
 import base64
 
+# Bootstrap --------------------------------------------------------------------------
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MATERIA],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}]
+                )
 
-app = dash.Dash(__name__)
+# Load Data --------------------------------------------------------------------------
 
-# -- Import and clean data (importing csv into pandas)--------------------------
-
-### Load company names to display in dropdown menu
+# Load company names to display in dropdown menu
 companylabels = pd.read_csv('data/companylabels.csv', usecols=['FullName', 'ShortForm'])
 companylabels = companylabels.values.tolist()
 
-### For global initiatives table 
+# For global initiatives table 
 initiatives_file = pd.read_csv('data/esg_initiatives.csv')
-
 # replace NaN for initiatives without acronym
 initiatives_file = initiatives_file.replace({np.nan: '-'})
-
 # dictionary: key-initiative spelled out fully, value-[acronym, description]
 initiatives_dict = initiatives_file.set_index('Initiative').T.to_dict('list')
+# read csv containing all initiatives
+all_initiative_array = pd.read_csv('data/all_initiatives.csv')
 
-# combine all initiatives from 4 FIs into one dataframe
-ab_initiatives = pd.read_csv('data/asian_banks_initiatives.csv')
-as_initiatives = pd.read_csv('data/asset_managers_initiatives.csv')
-ins_initiatives = pd.read_csv('data/insurance_initiatives.csv')
-pf_initiatives = pd.read_csv('data/pension_funds_initiatives.csv')
-all_initiative_array = pd.concat([ab_initiatives, as_initiatives, ins_initiatives, pf_initiatives])
-all_initiative_array = all_initiative_array.drop(all_initiative_array.columns[0], axis=1)
-all_initiative_array = all_initiative_array.reset_index(drop=True)
+# For WordCloud
+dfm = pd.DataFrame({'word': ['climate', 'emission', 'esg', 'investment', 'energy'], 'freq': [10, 8, 6, 4, 1]})
+def plot_wordcloud(data):
+    d = {a: x for a, x in data.values}
+    wc = WordCloud(background_color='white', width=480, height=360)
+    wc.fit_words(d)
+    return wc.to_image()
 
-### For word cloud
-dfm = pd.DataFrame({'word': ['apple', 'pear', 'orange'], 'freq': [1,3,9]})
+# For decarbonization rating
+ratings_file = pd.read_csv('data/all_percentile.csv', usecols=['name', 'percentile'])
 
-# ------------------------------------------------------------------------------
-# App layout
-app.layout = html.Div([
+# Cards --------------------------------------------------------------------------------
+card_sentiment = dbc.Card([
+                    dbc.CardBody([
+                        html.H5('Overall Sentiment Level', className='text-center'),
+                        daq.Gauge(id='sentiment_gauge',
+                                color={"gradient":True,"ranges":{"red":[0,3],"yellow":[3,4],"green":[4,5]}},
+                                value=3.6,
+                                max=5,
+                                min=0,
+                                className='d-flex justify-content-center',
+                                style={'height':200}
+                        )
+                    ])
+                ])     
 
-    html.H1("Decarbonisation Dashboard", style={'text-align': 'center'}),
+card_decarbonization_ratings = dbc.Card([
+                                    dbc.CardBody([
+                                        html.H5('Decarbonisation Ratings'),
+                                        html.H1(id='decarbonization_rating', children='')
+                                    ]),
+                                ], color='success', outline=True)
 
-    html.Div(id='output_container', children=[]),
+                                
+# Layout -----------------------------------------------------------------------------
+app.layout = dbc.Container([
+    # Declare rows: 4 Rows
+    # Row 1: For Header and dropdown
+    dbc.Row([
+        dbc.Col(html.H1('Decarbonization Dashboard',
+                        className='text-center, mb-4'),
+                width=12)
+    ]),
+
+    # Row 2: For Dropdown
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(id='company_dropdown',
+                        options=[{'label': x[0], 'value': x[1]} for x in companylabels],
+                        multi=False,
+                        value='AXA')
+        ], width={'size':5})
+    ]),
+
     html.Br(),
-    dcc.Dropdown(id="company",
-                 options=[{'label': x[0], 'value': x[1]} for x in companylabels],
-                 multi=False,
-                 value="AXA",
-                 style={'width': "50%"}
-                 ),
+    html.Br(),
 
-    dcc.Graph(id='initiative_table', figure={}),
-    html.Div([html.Img(id="image_wc")])
+    # Row 3: For Sentiment Gauge Chart, Big Number, BoxPlot
+    dbc.Row([
+        dbc.Col([card_sentiment], width={'size':3, 'offset':0, 'order':1}),
+        dbc.Col([card_decarbonization_ratings], width={'size':3, 'offset':0, 'order':2}),
+        dbc.Col([
+            dcc.Graph(id='boxplot', figure={})
+        ], width={'size':6, 'offset':0, 'order':3})
+    ]),
 
-])
+    # Row 4: For Global Initiatives Table & WordCloud
+    dbc.Row([
+        dbc.Col([
+            html.H5('Global Initiatives', className='text-center'),
+            dcc.Graph(id='initiative_table', figure={})
+        ], width={'size':7, 'offset':0, 'order':1}),
+        dbc.Col([
+            html.H5('WordCloud', className='text-center'),
+            dbc.CardImg(id='word_cloud')
+        ], width={'size':5, 'offset':0, 'order':2})
+    ])
+], fluid=True)
 
-
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
+# Callback -----------------------------------------------------------------------------
+# For Sentiment Gauge Chart
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='initiative_table', component_property='figure')],
-    [Input(component_id='company', component_property='value')]
+    Output(component_id='sentiment_gauge', component_property='value'),
+    Input(component_id='company_dropdown', component_property='value')
+)
+def update_output(value):
+    return value # !! need to change according to company
+
+# For Big Numbers 1: Decarbonization Rating
+@app.callback(
+    Output(component_id='decarbonization_rating', component_property='children'),
+    Input(component_id='company_dropdown', component_property='value')
+    )
+def update_output(company):
+    rating = round(ratings_file.set_index('name').percentile.loc[company])
+    return rating
+    
+
+# Global Initiatives Table
+@app.callback(
+    Output(component_id='initiative_table', component_property='figure'),
+    Input(component_id='company_dropdown', component_property='value')
 )
 def update_graph(option_slctd):
-    print(option_slctd) # for checking
-    print(type(option_slctd)) # for checking
-
-    container = "Select Company"
-
     company_initiative = all_initiative_array.set_index('Company').Initiatives.loc[option_slctd]
     company_initiative = ast.literal_eval(company_initiative)
     company_initiative = sorted(company_initiative)
@@ -92,10 +154,9 @@ def update_graph(option_slctd):
         data.append([full_name, acronym, details])
     
     df = pd.DataFrame(data, columns = ['Initiatives', 'Acronym', 'Details'])
-    rows = len(df.index)
 
     fig = go.Figure(data=[go.Table(
-        columnwidth = [130, 30, 380],
+        columnwidth = [100, 50, 400],
         header = dict(
             values=list(df.columns),
             font_color='rgb(100,100,100)',
@@ -103,28 +164,25 @@ def update_graph(option_slctd):
             align='left'),
         cells = dict(
             values=[df.Initiatives, df.Acronym, df.Details],
-            # 2-D list of colors for alternating rows
             fill_color='white', 
             font_color='rgb(100,100,100)',
             font_size=10,
             align='left',
-            height=20))
-    ])
+            height=20)),
+    ], layout=go.Layout(margin={'l':0, 'r':0, 't':10, 'b':10}))
 
-    return container, fig
+    return fig
 
-def plot_wordcloud(data):
-    d = {a: x for a, x in data.values}
-    wc = WordCloud(background_color='white', width=480, height=360)
-    wc.fit_words(d)
-    return wc.to_image()
-
-@app.callback(dd.Output('image_wc', 'src'), [dd.Input('image_wc', 'id')])
-def make_image(b):
+# For WordCloud
+@app.callback(
+    Output(component_id='word_cloud', component_property='src'),
+    Input(component_id='company_dropdown', component_property='value')
+)
+def make_image(option_slctd):
     img = BytesIO()
-    plot_wordcloud(data=dfm).save(img, format='PNG')
+    plot_wordcloud(data=dfm).save(img, format='PNG')  # !! need to change according to company
     return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
-# ------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
